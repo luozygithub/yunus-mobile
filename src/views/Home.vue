ef446a7e21f13a606192d9d1375da493dda57aae
city <109882297+cityflysky@users.noreply.github.com>
2023-01-02 17:26:26 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -350,20 +350,8 @@
         </div>
         <div class="swiper-container2 swiper ">
           <div class="swiper-wrapper">
-            <div class=" swiper-slide ">
-              <div
-                  class="w-[60px] h-[60px] flex justify-center items-center background-gradient rounded-full sm:w-[50px] sm:h-[50px]">
-                <svg class="w-[30px] h-[30px]" viewBox="0 0 30 30" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M15 4.75C9.34 4.75 4.75 9.339 4.75 15c0 5.66 4.59 10.25 10.25 10.25S25.25 20.66 25.25 15c0-5.661-4.59-10.25-10.25-10.25zM2.75 15C2.75 8.234 8.235 2.75 15 2.75S27.25 8.234 27.25 15c0 6.765-5.485 12.25-12.25 12.25S2.75 21.765 2.75 15z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M10.25 12.5a1 1 0 0 1 1-1h.012a1 1 0 1 1 0 2h-.012a1 1 0 0 1-1-1zm7.5 0a1 1 0 0 1 1-1h.012a1 1 0 1 1 0 2h-.012a1 1 0 0 1-1-1zm-6.575 5.535a1 1 0 0 1 1.414.015 3.375 3.375 0 0 0 4.822 0 1 1 0 0 1 1.428 1.4 5.375 5.375 0 0 1-7.678 0 1 1 0 0 1 .014-1.415z"
-                        fill="#fff"></path>
-                </svg>
-              </div>
-
+            <div class="swiper-slide ">
+              <img src="../assets/icon-autonomy.png" alt="">
               <div class="slide-title">
                 {{ $t('Autonomy') }}
               </div>
@@ -373,24 +361,7 @@
               </div>
             </div>
             <div class=" swiper-slide">
-              <div
-                  class="w-[60px] h-[60px] flex justify-center items-center background-gradient rounded-full sm:w-[50px] sm:h-[50px]">
-                <svg class="w-[30px] h-[30px]" viewBox="0 0 30 30" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M17.5 2.75a1 1 0 0 1 1 1v5a.25.25 0 0 0 .25.25h5a1 1 0 1 1 0 2h-5a2.25 2.25 0 0 1-2.25-2.25v-5a1 1 0 0 1 1-1z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M8.75 4.75a1.5 1.5 0 0 0-1.5 1.5V10a1 1 0 1 1-2 0V6.25a3.5 3.5 0 0 1 3.5-3.5h8.75a1 1 0 0 1 .707.293l6.25 6.25a1 1 0 0 1 .293.707v13.75a3.5 3.5 0 0 1-3.5 3.5H15a1 1 0 1 1 0-2h6.25a1.5 1.5 0 0 0 1.5-1.5V10.414L17.086 4.75H8.75z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M7.5 14.75a2.75 2.75 0 1 0 0 5.5 2.75 2.75 0 0 0 0-5.5zM2.75 17.5a4.75 4.75 0 1 1 9.5 0 4.75 4.75 0 0 1-9.5 0z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M5.912 20.292a1 1 0 0 1 .67 1.245l-1.21 4.034 1.68-.84a1 1 0 0 1 .895 0l1.68.84-1.21-4.034a1 1 0 1 1 1.916-.574l1.875 6.25a1 1 0 0 1-1.405 1.181L7.5 26.745l-3.303 1.65a1 1 0 0 1-1.405-1.181l1.875-6.25a1 1 0 0 1 1.245-.67z"
-                        fill="#fff"></path>
-                </svg>
-              </div>
+              <img src="../assets/icon-Unchangingconditions.png" alt="">
               <div class="slide-title">
                 {{ $t("Unchangingconditions") }}
               </div>
@@ -399,21 +370,7 @@
               </div>
             </div>
             <div class=" swiper-slide">
-              <div
-                  class="w-[60px] h-[60px] flex justify-center items-center background-gradient rounded-full sm:w-[50px] sm:h-[50px]">
-                <svg class="w-[30px] h-[30px]" viewBox="0 0 30 31" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M15 13.503a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-3.5 1.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M3.658 15.003c3.02 5.055 6.604 7.51 10.714 7.733a1 1 0 0 1-.108 1.998c-5.154-.28-9.35-3.491-12.632-9.234a1 1 0 0 1 0-.993C5.074 8.484 9.52 5.253 15 5.253s9.926 3.23 13.368 9.254a1 1 0 0 1 0 .992 27.71 27.71 0 0 1-1.7 2.636 1 1 0 1 1-1.614-1.18c.44-.602.87-1.252 1.288-1.951-3.168-5.301-6.957-7.75-11.342-7.75-4.385 0-8.174 2.449-11.342 7.75z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M26.957 20.546a1 1 0 0 1 0 1.415l-5 5a1 1 0 0 1-1.414 0l-2.5-2.5a1 1 0 0 1 1.414-1.415l1.793 1.793 4.293-4.293a1 1 0 0 1 1.414 0z"
-                        fill="#fff"></path>
-                </svg>
-              </div>
+              <img src="../assets/icon-Transparency.png" alt="">
               <div class="slide-title">
                 {{ $t('Transparency') }}
               </div>
@@ -422,18 +379,7 @@
               </div>
             </div>
             <div class=" swiper-slide">
-              <div
-                  class="w-[60px] h-[60px] flex justify-center items-center background-gradient rounded-full sm:w-[50px] sm:h-[50px]">
-                <svg class="w-[30px] h-[30px]" viewBox="0 0 31 31" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M6.011 5.029a3.5 3.5 0 0 1 2.475-1.026h12.5a2.25 2.25 0 0 1 2.25 2.25v2.75h.25a2.25 2.25 0 0 1 2.25 2.25v3.75a1 1 0 1 1-2 0v-3.75a.25.25 0 0 0-.25-.25h-15a3.5 3.5 0 0 1-1.5-.337v11.837a1.5 1.5 0 0 0 1.5 1.5h15a.25.25 0 0 0 .25-.25v-3.75a1 1 0 1 1 2 0v3.75a2.25 2.25 0 0 1-2.25 2.25h-15a3.5 3.5 0 0 1-3.5-3.5v-15A3.5 3.5 0 0 1 6.011 5.03zm.975 2.474a1.5 1.5 0 0 0 1.5 1.5h12.75v-2.75a.25.25 0 0 0-.25-.25h-12.5a1.5 1.5 0 0 0-1.5 1.5z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M20.986 16.003a1.5 1.5 0 0 0 0 3h4v-3h-4zm-2.474-.975a3.5 3.5 0 0 1 2.474-1.025h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a3.5 3.5 0 0 1-2.474-5.975z"
-                        fill="#fff"></path>
-                </svg>
-              </div>
+              <img src="../assets/icon-Fullautomation.png" alt="">
               <div class="slide-title">
                 {{ $t('Full automation') }}
               </div>
@@ -442,18 +388,7 @@
               </div>
             </div>
             <div class=" swiper-slide">
-              <div
-                  class="w-[60px] h-[60px] flex justify-center items-center background-gradient rounded-full sm:w-[50px] sm:h-[50px]">
-                <svg class="w-[30px] h-[30px]" viewBox="0 0 30 31" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M6.25 21.003a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-3.5 1.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0zm21-16.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-3.5 1.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M23.75 9.003a1 1 0 0 1 1 1v6.25a7.25 7.25 0 0 1-7.25 7.25h-1.336l2.043 2.043a1 1 0 0 1-1.414 1.415l-3.75-3.75a1 1 0 0 1 0-1.415l3.75-3.75a1 1 0 0 1 1.414 1.415l-2.043 2.042H17.5a5.25 5.25 0 0 0 5.25-5.25v-6.25a1 1 0 0 1 1-1zM11.793 3.046a1 1 0 0 1 1.414 0l3.75 3.75a1 1 0 0 1 0 1.415l-3.75 3.75a1 1 0 0 1-1.414-1.415l2.043-2.043H12.5a5.25 5.25 0 0 0-5.25 5.25v6.25a1 1 0 1 1-2 0v-6.25a7.25 7.25 0 0 1 7.25-7.25h1.336l-2.043-2.042a1 1 0 0 1 0-1.415z"
-                        fill="#fff"></path>
-                </svg>
-              </div>
+              <img src="../assets/icon-Decentralization.png" alt="">
 
               <div class="slide-title">
                 {{ $t('Decentralization') }}
@@ -464,18 +399,7 @@
               </div>
             </div>
             <div class="swiper-slide">
-              <div
-                  class="w-[60px] h-[60px] flex justify-center items-center background-gradient rounded-full sm:w-[50px] sm:h-[50px]">
-                <svg class="w-[30px] h-[30px]" viewBox="0 0 31 31" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M15.986 11.003a1.5 1.5 0 0 0-1.5 1.5v5a1.5 1.5 0 0 0 3 0v-5a1.5 1.5 0 0 0-1.5-1.5zm0-2a3.5 3.5 0 0 1 3.5 3.5v5a3.5 3.5 0 1 1-7 0v-5a3.5 3.5 0 0 1 3.5-3.5z"
-                        fill="#fff"></path>
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M15.986 4.753c-5.66 0-10.25 4.59-10.25 10.25 0 5.661 4.59 10.25 10.25 10.25 5.661 0 10.25-4.589 10.25-10.25 0-5.66-4.589-10.25-10.25-10.25zm-12.25 10.25c0-6.765 5.485-12.25 12.25-12.25 6.766 0 12.25 5.485 12.25 12.25 0 6.766-5.484 12.25-12.25 12.25-6.765 0-12.25-5.484-12.25-12.25z"
-                        fill="#fff"></path>
-                </svg>
-              </div>
+              <img src="../assets/icon-100%online.png" alt="">
               <div class="slide-title">
                 {{ $t('100% online') }}
               </div>
@@ -1163,7 +1087,10 @@ export default {
           display: flex;
           flex-direction: column;
           align-items: center;
-
+          img{
+            width: 4em;
+            height: 4em;
+          }
           .slide-title {
             font-size: 24px;
             font-weight: 600;
